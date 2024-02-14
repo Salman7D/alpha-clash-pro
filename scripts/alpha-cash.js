6845e69dac4cc58fa47fe55fd89c8a3b9d5fbdde
Salman7D <salmanrayhan753@gmail.com>
2024-02-14 20:34:38 +0600

    element

--- scripts/alpha-cash.js
+++ scripts/alpha-cash.js
@@ -26,12 +26,62 @@ function handleKeyBoardKeyUpEvent(event){
     if(playerPressed === expectedAlphabet){
         console.log("you get a point");
         console.log("you have pressed correctly", expectedAlphabet);
+
+        const currentScore = getTextElementValueById("current-score");
+        
+        const updatedScore = currentScore + 1;
+        setTextElementValueById("current-score", updatedScore);
+
+
+    
+    
+    
+    
+    // --------------------------------------
+    // update score:
+    // 1. get the current score
+    //     const currentScoreElement = document.getElementById("current-score");
+    //     const currentScoreText = currentScoreElement.innerText;
+    //     const currentScore = parseInt(currentScoreText);
+    //     console.log(currentScore);
+        
+
+    // // 2. increase the score by 1
+    //     const newScore = currentScore + 1;
+
+    // // 3. show the updated score
+    //     currentScoreElement.innerText = newScore;                                                                         
+
+        // start a new round 
+
         removeBackgroundColorById(expectedAlphabet);
         continueGame(); 
     }
 
     else{
         console.log("you missed. you lost a life");
+
+        const currentLife = getTextElementValueById("current-life");
+        const updatedLife = currentLife - 1;
+        setTextElementValueById("current-life", updatedLife);
+
+
+
+
+
+
+        // -----------------------------
+        // step - 1: get the current Life number
+        // const currentLifeElement = document.getElementById("current-life");
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+        
+        // // step - 2: reduce the life count
+        // const newLife = currentLife - 1;
+
+        // // step - 3: display the updated  life count
+        // currentLifeElement.innerText = newLife;
+
     }
 }
 
--- scripts/utility.js
+++ scripts/utility.js
@@ -18,6 +18,25 @@ function removeBackgroundColorById(elementId){
     element.classList.remove("bg-orange-400");
 }
 
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+
+
+
+
+
+
+
 function getRandomAlphabet(){
     const alphabetString = "abcdefghijklmopqrstuvwxyz";
     const alphabets = alphabetString.split("");
