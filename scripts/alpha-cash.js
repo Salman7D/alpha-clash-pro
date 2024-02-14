4568e52fdeadab81a592da89604758707f44b2fd
Salman7D <salmanrayhan753@gmail.com>
2024-02-14 10:45:23 +0600

    random alphabet

--- scripts/alpha-cash.js
+++ scripts/alpha-cash.js
@@ -1,3 +1,24 @@
-function play(){
-    console.log("fuck me");
+// function play(){
+//     // step-1: hide the home screen. to hide the screen add the class hidden to the home section
+//     const homeScreen = document.getElementById("home-screen");
+//     homeScreen.classList.add("hidden");
+    
+//     // show the playground
+//     const playgroundSection = document.getElementById("play-ground");
+//     playgroundSection.classList.remove("hidden");
+//     // console.log(playgroundSection.classList);
+// }
+
+function continueGame(){
+    // step - 1: generate a random alphabet
+    const alphabet = getRandomAlphabet();
+    console.log("your random alphabet",alphabet);
+
 }
+
+function play(){
+   hideElementById("home-screen"); 
+   showElementById("play-ground");
+   continueGame();
+}
+
